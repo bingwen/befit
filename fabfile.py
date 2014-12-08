538cb02a1fcdd0cc0682dca29b94769e0a2e4d0e
bingwen <bingwenst@foxmail.com>
2014-12-08 10:49:32 +0800

    add fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,59 +1,42 @@
 from fabric.api import run, local, task, env
 
-env.name = "shae"
-env.user = "inad"
-env.env = "shae"
-env.path = "/home/inad/apps/shae"
-env.hosts = ['x.inad.com:2201']
+env.name = "befit"
+env.user = "bingwen"
+env.env = "befit"
+env.path = "/home/bingwen/apps/befit"
+env.hosts = ['befit.sbw.me']
 
 
 def prepare_deploy():
     local("echo ------------------------")
-    local("echo DEPLOYING Shae TO PRODUCTION")
+    local("echo DEPLOYING %S TO PRODUCTION" % env.name)
     local("echo ------------------------")
 
 
 @task
 def checkout_latest():
     """Pull the latest code into the git repo and copy to a timestamped release directory"""
-    import time
-    env.release = time.strftime('%Y%m%d%H%M%S')
-    run("cd %(path)s/repository; git pull origin master" % env)
-    run('cp -R %(path)s/repository %(path)s/releases/%(release)s; rm -rf %(path)s/releases/%(release)s/.git*' % env)
-    run('cd %(path)s/releases/%(release)s; cp %(path)s/conf/local_config.py local_config.py' % env)
+    run("cd %(path)s; git pull origin master" % env)
 
 
 def install_requirements():
     """Install the required packages using pip"""
-    run('cd %(path)s/releases/%(release)s; pyenv shell %(env)s; pip install -r requirements.txt' % env)
+    run('cd %(path)s; pyenv shell %(env)s; pip install -r requirements.txt' % env)
 
 
 def migrate():
     """Run our migrations"""
-    run('cd %(path)s/releases/%(release)s; pyenv shell %(env)s; python manage.py db upgrade' % env)
+    run('cd %(path)s; pyenv shell %(env)s; python manage.py db upgrade' % env)
 
 
 def symlink_current_release():
     """Symlink our current release, uploads and settings file"""
-    run('cd %(path)s; rm releases/previous; mv releases/current releases/previous;' % env)
-    run('cd %(path)s; ln -s %(release)s releases/current' % env)
+    pass
 
 
 @task
 def restart_server():
-    run("supervisorctl restart shae")
-
-
-@task
-def rollback():
-    """
-    Limited rollback capability. Simple loads the previously current
-    version of the code. Rolling back again will swap between the two.
-    """
-    run('cd %(path)s; mv releases/current releases/_previous;' % env)
-    run('cd %(path)s; mv releases/previous releases/current;' % env)
-    run('cd %(path)s; mv releases/_previous releases/previous;' % env)
-    restart_server()
+    run("supervisorctl restart %s" % env.name)
 
 
 @task
